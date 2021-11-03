f7be2f1276eeb697784f11217848dd67cc4b81ca
Diana Grande <granded@uci.edu>
2021-11-03 19:31:46 +0000

    Add add_mult and add_cubes functions

--- arithmetic.py
+++ arithmetic.py
@@ -1,10 +1,8 @@
 """Functions for common math operations."""
 
-
 def add(num1, num2):
     """Return the sum of the two inputs."""
     return num1 + num2
-
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
@@ -33,3 +31,31 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Get the sum of num1 and num2, then multiply sum with num3."""
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Add the cubes of num1 and num2"""
+    return (num1**3) + (num2**3) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
